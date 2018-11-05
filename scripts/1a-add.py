72c48a4639eee6913cf1a8f0cae44b229c09f2b4
Jerem0703 <jeremy.lesprit@ynov.com>
2018-11-05 22:02:08 +0100

    Create 1a-add.py

---
+++ scripts/1a-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3.4
+#1a-add.py
+#entrée utilisateur
+#LESPRIT Jérémy
+#16/10/2018
+
+premiernombre=int(input( "Entrez un nombre: " ))
+deuxièmenombre=int(input( "Entrez un autre nombre: "))
+Résultat=premiernombre+deuxièmenombre
+print(Résultat) 
