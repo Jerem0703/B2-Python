9cae863d1ee416b0d453a3c94a5f5162614c5764
Jerem0703 <jlesprit@me.com>
2018-11-11 13:50:18 +0100

    Update scripts/2a-mol/2a-mol.py

---
+++ scripts/2a-mol/2a-mol.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3.7
+#2a-mol.py
+#jeu du plus ou moins
+#LESPRIT Jérémy 
+#04/11/2018
+
+from random import randint
+
+from plusoumoins.helpers import print_into_file, file_input
+
+
+def get_answer(nombreAleatoire, nombrePropose):
+    assert isinstance(nombreAleatoire, int)
+    assert isinstance(nombrePropose, int)
+    if nombreAleatoire == nombrePropose:
+        rep = "Bravo"
+    elif nombreAleatoire > nombrePropose:
+        rep = "Trop grand"
+    elif nombreAleatoire < nombrePropose:
+        rep = "Trop petit"
+    else:
+        rep = "Erreur"
+    return rep
+
+
+MIN, MAX = 0, 100
+NUMBER = randint(MIN, MAX)
+FILE = "/myfile"
+
+i = 0
+user_input = ''
+print_into_file(FILE, user_input)
+while True:
+    try:
+        user_input = int(file_input(FILE, str(user_input)))
+    except ValueError:
+        continue
+    answer = get_answer(user_input, NUMBER)
+    i += 1
+print_into_file(FILE, answer)
